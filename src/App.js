01a207bd42de10175f4b2d020203e9974f0acf50
ridoy113 <almamounhridoy113@gmail.com>
2022-09-26 16:48:45 +0600

    install some dependensi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-
+      <Router>
+        <Switch>
+          <Route path='/'></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
