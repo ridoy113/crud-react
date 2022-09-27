217439749e7b9a12599c581df3b7748c198d704b
ridoy113 <almamounhridoy113@gmail.com>
2022-09-27 15:28:25 +0600

    make a table

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,42 @@
 import React from 'react';
-import { Button } from 'react-bootstrap'
+import { Button, Table } from 'react-bootstrap'
 
 
 const Home = () => {
     return (
         <div>
             <Button variant="primary">Create User</Button>
+
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>@fat</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td colSpan={2}>Larry the Bird</td>
+                        <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </Table>
+
         </div>
     );
 };
