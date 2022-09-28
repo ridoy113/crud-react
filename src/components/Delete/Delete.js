e724ca70a66dc583c3ca3221e4acc94fcb6b7f99
ridoy113 <almamounhridoy113@gmail.com>
2022-09-28 15:33:23 +0600

    delete button making delete data

--- src/components/Delete/Delete.js
+++ src/components/Delete/Delete.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { UserContext } from '../UserContext/UserContext';
 import './Delete.css'
 
 const Delete = () => {
+
+    const [users, setUsers] = useContext(UserContext);
+    const { id } = useParams();
+    
+    const user = users.filter(user => user.id != id);
+
     return (
         <div>
             <h1>Delete Mama</h1>
