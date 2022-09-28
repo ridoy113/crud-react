fe24d041762af369fc60ccb9624c6d1e26d6e51e
ridoy113 <almamounhridoy113@gmail.com>
2022-09-28 15:47:17 +0600

    delete button working fantastic

--- src/components/Delete/Delete.js
+++ src/components/Delete/Delete.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { useParams } from 'react-router-dom';
+import { Button, Modal } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
 import { UserContext } from '../UserContext/UserContext';
 import './Delete.css'
 
@@ -7,12 +8,28 @@ const Delete = () => {
 
     const [users, setUsers] = useContext(UserContext);
     const { id } = useParams();
-    
-    const user = users.filter(user => user.id != id);
+
+    const deleteUser = (id) => {
+        const user = users.filter(user => user.id != id);
+        setUsers(user)
+    }
 
     return (
         <div>
-            <h1>Delete Mama</h1>
+            <Modal.Dialog>
+                <Modal.Header closeButton>
+                    <Modal.Title>Are You Sure?</Modal.Title>
+                </Modal.Header>
+
+                <Modal.Footer>
+                    <Link to='/'>
+                        <Button className='delete_btn' variant="info">Cancel</Button>
+                        <Button
+                            onClick={() => deleteUser(id)}
+                            variant="danger">Delete</Button>
+                    </Link>
+                </Modal.Footer>
+            </Modal.Dialog>
         </div>
     );
 };
