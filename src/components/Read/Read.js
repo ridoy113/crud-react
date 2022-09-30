b465b8c6e3080e2a676e978d9f3846e222646869
ridoy113 <almamounhridoy113@gmail.com>
2022-09-30 15:29:56 +0600

    load data for read page

--- src/components/Read/Read.js
+++ src/components/Read/Read.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../UserContext/UserContext';
 import './Read.css'
 
 
 const Read = () => {
-    return (
-        <div className='reaad'>
 
+    const [users, setUsers] = useContext(UserContext);
+    const { id } = useParams();
+
+    const user = users.filter(user => user.id == id);
+    console.log(user);
+
+    return (
+        <div className='read'>
+            <h1>ID : {user[0].id}</h1>
+            <h1>Name : {user[0].name}</h1>
+            <h1>Position : {user[0].position}</h1>
+            <h1>Salary : {user[0].salary}</h1>
+            <Link to="/">
+                <Button variant="info">
+                    Back To Home
+                </Button>
+            </Link>
         </div>
     );
 };
