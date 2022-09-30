fed6a662ec729093715c7cd19e08d52a60fe0bea
ridoy113 <almamounhridoy113@gmail.com>
2022-09-30 15:49:43 +0600

    make a from in create page

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import { UserContext } from '../UserContext/UserContext';
 import './Create.css'
 
@@ -9,7 +11,58 @@ const Create = () => {
 
     return (
         <div>
-            
+
+            <Form>
+                <Form.Group>
+                    <Form.Label>ID</Form.Label>
+                    <Form.Control
+                    // type="text"
+                    // name="id"
+                    // value={id}
+                    // onChange={updateId}
+                    // placeholder="Enter ID"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                    // type="text"
+                    // name="name"
+                    // value={name}
+                    // onChange={updateName}
+                    // placeholder="Enter Name"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Position</Form.Label>
+                    <Form.Control
+                    // type="text"
+                    // name="position"
+                    // value={position}
+                    // onChange={updatePosition}
+                    // placeholder="Enter Position"
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Salary</Form.Label>
+                    <Form.Control
+                    // type="text"
+                    // name="salary"
+                    // value={salary}
+                    // onChange={updateSalary}
+                    // placeholder="Enter Salary"
+                    />
+                </Form.Group>
+                <Button className="action_btn" variant="primary" type="submit">
+                    Create User
+                </Button>
+                <Link to="/">
+                    <Button className="action_btn" variant="info">
+                        Back to Home
+                    </Button>
+                </Link>
+            </Form>
+
         </div>
     );
 };
