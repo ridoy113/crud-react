67241aa258dc4688c47bab22ea5795d5c4f2de7b
ridoy113 <almamounhridoy113@gmail.com>
2022-09-30 16:07:11 +0600

    create page complite and working good

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../UserContext/UserContext';
@@ -9,58 +9,89 @@ const Create = () => {
 
     const [users, setUsers] = useContext(UserContext);
 
-    return (
-        <div>
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
+    const [position, setPosition] = useState("");
+    const [salary, setSalary] = useState("");
 
-            <Form>
+
+    const updateId = (e) => {
+        setId(e.target.value);
+    };
+
+    const updateName = (e) => {
+        setName(e.target.value);
+    };
+
+    const updatePosition = (e) => {
+        setPosition(e.target.value);
+    };
+
+    const updateSalary = (e) => {
+        setSalary(e.target.value);
+    };
+
+    const addUser = (e) => {
+        e.preventDefault();
+        setUsers([...users, { id: id, name: name, position: position, salary: salary }])
+    }
+
+
+    return (
+        <div className='create'>
+
+            <Form onSubmit={addUser}>
                 <Form.Group>
                     <Form.Label>ID</Form.Label>
                     <Form.Control
-                    // type="text"
-                    // name="id"
-                    // value={id}
-                    // onChange={updateId}
-                    // placeholder="Enter ID"
+                        type="text"
+                        name="id"
+                        value={id}
+                        onChange={updateId}
+                        placeholder="Enter ID"
                     />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Name</Form.Label>
                     <Form.Control
-                    // type="text"
-                    // name="name"
-                    // value={name}
-                    // onChange={updateName}
-                    // placeholder="Enter Name"
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={updateName}
+                        placeholder="Enter Name"
                     />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Position</Form.Label>
                     <Form.Control
-                    // type="text"
-                    // name="position"
-                    // value={position}
-                    // onChange={updatePosition}
-                    // placeholder="Enter Position"
+                    type="text"
+                    name="position"
+                    value={position}
+                    onChange={updatePosition}
+                    placeholder="Enter Position"
                     />
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Salary</Form.Label>
                     <Form.Control
-                    // type="text"
-                    // name="salary"
-                    // value={salary}
-                    // onChange={updateSalary}
-                    // placeholder="Enter Salary"
+                        type="text"
+                        name="salary"
+                        value={salary}
+                        onChange={updateSalary}
+                        placeholder="Enter Salary"
                     />
                 </Form.Group>
-                <Button className="action_btn" variant="primary" type="submit">
+
+                <Button className="create" variant="primary" type="submit">
                     Create User
                 </Button>
+
                 <Link to="/">
-                    <Button className="action_btn" variant="info">
+                    <Button className="create" variant="info">
                         Back to Home
                     </Button>
                 </Link>
+
             </Form>
 
         </div>
